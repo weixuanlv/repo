8e2b40e48f6ce50edb69ea3e8ba23410da56fe56
root <root@163.com>
2019-09-23 17:06:54 +0800

    修改 clone

--- git_test1/src/com/itheima/git/FirstDemo.java
+++ git_test1/src/com/itheima/git/FirstDemo.java
@@ -7,11 +7,15 @@ public class FirstDemo {
         System.out.println("hello....git");
         System.out.println("hello....git");
         System.out.println("hello....git");
+        //护手霜
     }
 
     public void test1(){
         int i =1;
         i+=10;
         System.out.println(i);
+
+        int p =2;
+        p+=10;
     }
 }
